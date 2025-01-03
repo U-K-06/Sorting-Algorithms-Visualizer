373c13f4075ef87351aaf83fcc185397cf3e69c5
U-K-06 <utkarsh7khajuria8@gmail.com>
2025-01-03 19:27:10 +0530

    added insertion-sort and changed file name to algorithms

--- src/Algorithms/Algorithms.cpp
+++ src/Algorithms/Algorithms.cpp
@@ -18,3 +18,24 @@ void bubble_sort(std::vector<int>& arr)
     }
 }
 
+
+void insertion_sort(std::vector<int>& arr)
+{
+    for(int i  = 1;i<arr.size();i++)
+    {
+        int compared = arr[i];
+        int j = i-1;
+
+        while (j>=0 && compared < arr[j])
+        {
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = compared;
+    }
+    }
+
+
+
+
+
--- src/Graph_Generation/main.py
+++ src/Graph_Generation/main.py
@@ -1,2 +1,8 @@
+import numpy as np
+import pandas as pd
+import plotly.express as px
 
 
+def read_and_process_data(file_path: str) -> list:
+    data = pd.read_csv(file_path)
+    return [i for i in data['array']]
